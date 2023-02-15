7c3b86fce94a07ed2abe0e71e8cf739db942c8fc
mohamedmosilhy <14712020100557@stud.cu.edu.eg>
2023-02-15 21:11:29 +0200

    adding UI with HTML, styling this UI with CSS, and manipulating this UI with javascript.

--- game.js
+++ game.js
@@ -6,33 +6,39 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
 
     if(playerSelection === "rock" && computerSelection === "scissors")
-    {
-        return "player";
+    {   
+        playerScore++;
+        return "You Win! Rock beats Scissors";
     }
     
     else if(playerSelection === "rock" && computerSelection === "paper")
-    {
-        return "computer";
+    {   
+        computerScore++;
+        return "You Lose! Paper beats Rock";
     }
 
     else if(playerSelection === "scissors" && computerSelection === "rock")
     {
-        return "computer";
+        computerScore++;
+        return "You Lose! Rock beats Scissors";
     }
     
     else if(playerSelection === "scissors" && computerSelection === "paper")
     {
-        return "player";
+        playerScore++;
+        return "You Win! Scissors beats Paper";
     }
 
     else if(playerSelection === "paper" && computerSelection === "rock")
-    {
-        return "player";
+    {   
+        playerScore++;
+        return "You Win! Paper beats Rock";
     }
     
     else if(playerSelection === "paper" && computerSelection === "scissors")
     {
-        return "computer";
+        computerScore++;
+        return "You Lose! Scissors beats Paper";
     }
 
     else
@@ -40,40 +46,60 @@ function playRound(playerSelection, computerSelection){
         return "it's a tie";
     }
 }
-
-function game(){
-    let playerSelection;
-    let computerSelection;
-    let roundResult;
-    let playerScore = 0,computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("enter your choice").toLowerCase();
-        computerSelection = getComputerChoice().toLowerCase();
-        roundResult = playRound(playerSelection,computerSelection);
-
-        if(roundResult === "player"){
-            playerScore++;
-        }
-        else if(roundResult === "computer"){
-            computerScore++;
-        }
-        console.log(roundResult)
-    }
-
-    if(playerScore > computerScore)
-    {
-        return "player is the winner.";
-    }
-    else if(playerScore < computerScore)
-    {
-        return "computer is the winner.";
-    }
-    else
-    {
-        return "it's a tie.";
-    }
-}
+let playerScore = 0,computerScore = 0;
 
 
-console.log(game());
+let computerSelection ="";
+let playerSelection = "";
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+
+const result = document.querySelector(".result");
+
+const playerChoice = document.querySelector('.playerChoice');
+const comChoice = document.querySelector('.comChoice')
+
+const playerScoreNode = document.querySelector(".playerScore")
+const comScoreNode = document.querySelector(".comScore")
+
+rockButton.addEventListener('click',() => {
+    playerSelection = "Rock"
+    playerChoice.textContent = playerSelection;
+    computerSelection = getComputerChoice();
+    comChoice.textContent = computerSelection;
+    result.textContent = playRound(playerSelection.toLowerCase(),computerSelection.toLowerCase());
+    playerScoreNode.textContent = playerScore;
+    comScoreNode.textContent = computerScore;
+    
+});
+
+paperButton.addEventListener('click',() => {
+    playerSelection = "Paper"
+    playerChoice.textContent = playerSelection;
+    computerSelection = getComputerChoice();
+    comChoice.textContent = computerSelection;
+    result.textContent = playRound(playerSelection.toLowerCase(),computerSelection.toLowerCase());
+    playerScoreNode.textContent = playerScore;
+    comScoreNode.textContent = computerScore;
+    
+});
+
+scissorsButton.addEventListener('click',() => {
+    playerSelection = "Scissors"
+    playerChoice.textContent = playerSelection;
+    computerSelection = getComputerChoice();
+    comChoice.textContent = computerSelection;
+    result.textContent = playRound(playerSelection.toLowerCase(),computerSelection.toLowerCase());
+    playerScoreNode.textContent = playerScore;
+    comScoreNode.textContent = computerScore;
+    
+});
+
+
+
+
+
+
+
+
